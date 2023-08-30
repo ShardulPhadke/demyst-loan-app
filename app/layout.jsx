134bbff207fd131e165510c18b9ea598bed5da00
ShardulPhadke <shardul1407@gmail.com>
2023-08-30 19:23:09 +0530

    login page

---
+++ app/layout.jsx
@@ -0,0 +1,34 @@
+import '@styles/globals.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+import { title } from 'process'
+import { Children } from 'react'
+
+export const metadata = {
+    title: "LoanShark",
+    description: "Get your loan approved quickly!"
+}
+
+const RootLayout = ({children}) => {
+
+  return (
+    <html lang='en'>
+        <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider> 
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
